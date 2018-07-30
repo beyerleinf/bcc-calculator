69bd9175ce1e83e89608a74103d44f9d67054cb2
beyerleinf <mail@fabian-b.de>
2018-07-30 16:20:39 +0200

    Migrated to node-bcc and added ASCII string mode

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import {FormsModule} from '@angular/forms';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './components/app/app.component';
+import { TutorialComponent } from './components/tutorial/tutorial.component';
 
-@NgModule({declarations: [AppComponent], imports: [BrowserModule, FormsModule], providers: [], bootstrap: [AppComponent]})
+@NgModule({declarations: [AppComponent, TutorialComponent], imports: [BrowserModule, FormsModule], providers: [], bootstrap: [AppComponent]})
 export class AppModule {}
--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {Bcc, Hex, ObjectHelper} from '../../helper';
+import {Ascii, Bcc, Hex} from 'node-bcc';
 
 /**
  * The main component of the app.
@@ -10,20 +10,12 @@ import {Bcc, Hex, ObjectHelper} from '../../helper';
 @Component({selector: 'app-root', templateUrl: './app.component.html', styleUrls: ['./app.component.css']})
 export class AppComponent {
   /**
-   * THe {@link Bcc} helper class instance.
+   * The input mode selected by the user. Can be either `ascii` or `hex`.
    *
-   * @type {Bcc}
+   * @type {string}
    * @memberof AppComponent
    */
-  bcc: Bcc;
-
-  /**
-   * The {@link Hex} helper class instance.
-   *
-   * @type {Hex}
-   * @memberof AppComponent
-   */
-  hex: Hex;
+  inputMode: string;
 
   /**
    * The HEX string input.
@@ -32,6 +24,14 @@ export class AppComponent {
    * @memberof AppComponent
    */
   hexString: string;
+
+  /**
+   * The ASCII string input.
+   *
+   * @type {string}
+   * @memberof AppComponent
+   */
+  asciiString: string;
 
   /**
    * The calculated BCC value for the HEX input.
@@ -62,8 +62,7 @@ export class AppComponent {
    * @memberof AppComponent
    */
   constructor() {
-    this.hex = new Hex();
-    this.bcc = new Bcc(this.hex);
+    this.inputMode = 'ascii';
   }
 
   /**
@@ -72,18 +71,26 @@ export class AppComponent {
    * @memberof AppComponent
    */
   calculate() {
-    if (ObjectHelper.checkString(this.hexString)) {
-      if (this.hex.validate(this.hexString.replace(/ /g, ''))) {
-        const splitHexString = this.hex.split(this.hexString.replace(/ /g, ''));
-        const bccResult = this.bcc.calculate(splitHexString);
-        this.bccVal = this.hex.toHexString([bccResult], true);
+    if (this.inputMode === 'ascii') {
+      if (Ascii.validate(this.asciiString)) {
+        const bytes = Ascii.asciiToByteArray(this.asciiString);
+        const bccResult = Bcc.calculate(bytes);
+        this.bccVal = Hex.toHexString([bccResult], true);
         this.bccBinary = bccResult.toString(2);
         this.error = null;
       } else {
-        this.error = 'This is not a valid hex string.';
+        this.error = 'You did not enter a valid ASCII string.';
       }
-    } else {
-      this.error = 'You did not enter anything.';
+    } else if (this.inputMode === 'hex') {
+      if (Hex.validate(this.hexString.replace(/ /g, ''))) {
+        const splitHexString = Hex.split(this.hexString.replace(/ /g, ''));
+        const bccResult = Bcc.calculate(splitHexString);
+        this.bccVal = Hex.toHexString([bccResult], true);
+        this.bccBinary = bccResult.toString(2);
+        this.error = null;
+      } else {
+        this.error = 'You did not enter a valid hex string.';
+      }
     }
   }
 }
