67dee4e44241eea1cd662621f1bf4639f0df0e6a
Fabian Beyerlein <mail@fabian-b.de>
2018-09-14 23:33:57 +0200

    chore(tests): Added average spec time reporter

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,7 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-module.exports = function (config) {
+module.exports = function(config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
@@ -10,23 +10,48 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-firefox-launcher'),
       require('karma-jasmine-html-reporter'),
+      require('karma-spec-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-average-spec-time-reporter'),
     ],
-    client:{
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    client: {
+      clearContext: false  // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, 'coverage'), reports: [ 'json', 'html' ],
-      fixWebpackSourcePaths: true
+      dir: require('path').join(__dirname, 'coverage'),
+      reports: ['json', 'html'],
+      fixWebpackSourcePaths: true,
     },
-    
-    reporters: ['progress', 'kjhtml'],
+    specReporter: {
+      maxLogLines: 15,
+      suppressErrorSummary: true,
+      suppressFailed: false,
+      suppressPassed: false,
+      suppressSkipped: true,
+      showSpecTiming: true,
+      failFast: true,
+    },
+    specTimeReporter: {
+      showBrowserId: true,
+      showLongestSpec: true,
+      enableThresholds: true,
+      max: 500,
+      warn: 250,
+    },
+    reporters: [
+      'spec',
+      'kjhtml',
+      'spec-time',
+    ],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome', 'Firefox'],
+    browsers: [
+      'Chrome',
+      'Firefox',
+    ],
     singleRun: true
   });
 };
