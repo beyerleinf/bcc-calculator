a143902a2f78026cf58b2ce4c68b03fa394c3f8e
Fabian Beyerlein <dev@beyerleinf.de>
2021-06-13 14:19:40 +0200

    chore: Update Angular configs

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -3,35 +3,31 @@ import { Ascii, Bcc, Hex } from 'node-bcc';
 
 @Component({ selector: 'app-root', templateUrl: './app.component.html', styleUrls: ['./app.component.css'] })
 export class AppComponent {
-  inputMode: string;
-  hexString: string;
-  asciiString: string;
-  bccVal: string;
-  bccBinary: string;
-  error: string;
-
-  constructor() {
-    this.inputMode = 'ascii';
-  }
+  inputMode = 'ascii';
+  hexString = '';
+  asciiString = '';
+  bccVal = '';
+  bccBinary = '';
+  error = '';
 
   calculate() {
     if (this.inputMode === 'ascii') {
       if (Ascii.validate(this.asciiString)) {
         const bytes = Ascii.asciiToByteArray(this.asciiString);
         const bccResult = Bcc.calculate(bytes);
-        this.bccVal = Hex.toHexString([bccResult], true);
+        this.bccVal = Hex.toHexString([bccResult], true) || '';
         this.bccBinary = bccResult.toString(2);
-        this.error = null;
+        this.error = '';
       } else {
         this.error = 'You did not enter a valid ASCII string.';
       }
     } else if (this.inputMode === 'hex') {
       if (Hex.validate(this.hexString.replace(/ /g, ''))) {
-        const splitHexString = Hex.split(this.hexString.replace(/ /g, ''));
+        const splitHexString = Hex.split(this.hexString.replace(/ /g, '')) || [];
         const bccResult = Bcc.calculate(splitHexString);
-        this.bccVal = Hex.toHexString([bccResult], true);
+        this.bccVal = Hex.toHexString([bccResult], true) || '';
         this.bccBinary = bccResult.toString(2);
-        this.error = null;
+        this.error = '';
       } else {
         this.error = 'You did not enter a valid hex string.';
       }
